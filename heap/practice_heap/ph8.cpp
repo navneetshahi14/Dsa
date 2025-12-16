6ba6fc15f428cfed14c0784eae16779fe9adeebe
navneetshahi14 <navneet.shahi2004@gmail.com>
2025-12-16 21:58:58 +0530

    heap

---
+++ heap/practice_heap/ph8.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long minCost(long long arr[],long long n){
+    priority_queue<long long, vector<long long>, greater<long long>> pq;
+
+    for(int i = 0;i<n;i++){
+        pq.push(arr[i]);
+    }
+    long long cost = 0;
+
+    while(pq.size() > 1){
+        long long a = pq.top();
+        pq.pop();
+
+        long long b = pq.top();
+        pq.pop();
+
+        long long sum = a + b;
+        cost += sum;
+
+        pq.push(sum);
+    }
+
+    return cost;
+}
+
+int main() {
+
+    long long arr[] = {4, 3, 2, 6};
+    long long n = 4;
+
+    cout<<"The answer is "<<minCost(arr,n)<<endl;
+ 
+return 0;
+}
